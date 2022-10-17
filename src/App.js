3f7d44dfab6736eaf9c65025be799e5ba0874b8c
Braden Jensen <18bjensen3@gmail.com>
2022-10-17 15:28:02 -0600

    updated website

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
+import DevProjects from "./components/DevProjects";
 import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
+import VidProjects from "./components/VidProjects";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
-      <Projects />
+      <DevProjects />
       <Skills />
-      <Testimonials />
+      <VidProjects />
       <Contact />
     </main>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,21 +11,21 @@ export default function Navbar() {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
+          <a href="#devprojects" className="mr-5 hover:text-white">
+            Development Projects
           </a>
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
+          <a href="#vidprojects" className="mr-5 hover:text-white">
+            Video Projects
           </a>
         </nav>
         <a
           href="#contact"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
         >
-          Hire Me
+          Contact Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
